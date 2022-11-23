b82360b49af416db335b031708d18875d623ec31
DimasVhigarr <dimasvhigar2002@gmail.com>
2022-11-23 15:46:24 +0700

    menambahkan function public bool search

--- Exercise_linked_list/Program.cs
+++ Exercise_linked_list/Program.cs
@@ -8,6 +8,7 @@ namespace Exercise_linked_list
 {
     class Node
     {
+        /*create node for the circular nexted list*/
         public int rollNumber;
         public string name;
         public Node next;
@@ -19,6 +20,19 @@ namespace Exercise_linked_list
         public CircularList()
         {
             LAST = null;
+        }
+
+        public bool Search(int rollNo, ref Node previous, ref Node current)/*search for the specified node*/
+        {
+            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true);/*return true if the node is found*/
+            }
+            if (rollNo == LAST.rollNumber)/* if the node is present at the end*/
+                return true;
+            else
+                return (false);/* return false if the node is not found*/
         }
     }
     internal class Program

23296b99a602b2704111bfed2a7d60d7dba8b7e0
DimasVhigarr <dimasvhigar2002@gmail.com>
2022-11-23 16:12:37 +0700

    menambahkan case 2

--- Exercise_linked_list/Program.cs
+++ Exercise_linked_list/Program.cs
@@ -88,6 +88,27 @@ namespace Exercise_linked_list
                                 obj.traverse();
                             }
                             break;
+                        case '2':
+                            {
+                                if (obj.listEmpty() == true)
+                                {
+                                    Console.WriteLine("\nList is empty");
+                                    break;
+                                }
+                                Node prev, curr;
+                                prev = curr = null;
+                                Console.Write("\nEnter the roll number of the student whose record is to be searched: ");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                if (obj.Search(num, ref prev, ref curr) == false)
+                                    Console.WriteLine("\nRecord not found");
+                                else
+                                {
+                                    Console.WriteLine("\nRecord found");
+                                    Console.WriteLine("\nRoll number: " + curr.rollNumber);
+                                    Console.WriteLine("\nName: " + curr.name);
+                                }
+                            }
+                            break;
                     }
                 }
             }
